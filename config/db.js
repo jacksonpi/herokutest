bb3ea0c90c1d85204a92b4ccd9bb0861adc8c088
Jackson <jacksonpihk@gmail.com>
2020-01-23 15:46:41 -0300

    Adicionando login correcao

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
-if(process.env.NODE_ENV == "production"){
+/*if(process.env.NODE_ENV == "production"){
    module.exports = {mongoURI:"mongodb+srv://jacksonblogapp:k2b7nm25@cluster0-ylzgp.mongodb.net/test?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI:"mongodb://localhost/herokutest"}
-}
+}*/
+
+module.exports = {mongoURI:"mongodb+srv://jacksonblogapp:k2b7nm25@cluster0-ylzgp.mongodb.net/test?retryWrites=true&w=majority"}
